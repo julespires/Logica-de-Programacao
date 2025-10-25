1d141af94e402f3da8c74b3025e258ab1adb9959
Jules Pires <julespires@hotmail.com>
2025-10-25 16:16:45 -0300

    Exercícios

---
+++ Ex-002.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas - vindas.
+Autor:Jules do Nascimento Pires
+Ex:002
+Data:02/10/2025
+'''
+nome = str(input("Qual é o seu nome?"))
+
+print("Olá,{} é um prazer te conhecer!".format(nome))
---
+++ Ex-003.py
@@ -0,0 +1,16 @@
+'''
+Crie um programa que leia dois números e mostre a soma entre eles.
+Autor:Jules do Nascimento Pires
+Ex:003
+Data:02/10/2025
+'''
+
+# Entrada dos valores
+num1 = int(input("Primeiro número:"))
+num2 = int(input("Segundo número:"))
+
+# Realiza a soma
+soma = num1 + num2
+
+# Mostra o resultado da soma
+print("A soma entre {} + {} = {}".format(num1, num2, soma))
