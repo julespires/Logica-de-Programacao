3e23d070d99ee8550c5335dc7a5503289ca99506
Jules Pires <julespires@hotmail.com>
2025-10-31 08:51:36 -0300

    Exercicios

--- 1-Estrutura Sequencial/Ex-001.py
+++
@@ -1,8 +0,0 @@
-'''
-Faça um programa que imprima a mensagem "Olá Mundo!" para o usuário
-Autor:Jules do Nascimento Pires
-Ex-001
-Data:22/10/2025
-'''
-
-print("Olá, Mundo!")
--- Ex-006.py
+++ Ex-006.py
@@ -2,16 +2,16 @@
 Faça um programa que receba o salário de um funcionário, calcule e mostre o novo salário, sabendo-se
 que este sofreu um aumento de 25%.
 Autor:Jules do Nascimento Pires
-Ex:004
-Data:23/10/2025
+Ex:006
 '''
 
-# Entrada do salario
-salario = float(input("Informe o salário:"))
+# Entrada do salário do funcionáriio
+salario = float(input("Salário do funcionário: R$"))
 
-# Calcula o novo aumento e o novo salário
+# Calcula o aumento e o novo salário
 aumento = salario * 25 / 100
 novoSalario = salario + aumento
 
-# Mostra o novo salário 
-print("Novo salário com aumento de 25%:{}".format(novoSalario))
+# Mostra o novo salário
+print("Novo salário:: R${:.2f}".format(novoSalario))
+
