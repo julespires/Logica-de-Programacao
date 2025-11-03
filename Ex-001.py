94817fbaf159eaba88201858c4cfe1bfb3e4796d
Jules Pires <julespires@hotmail.com>
2025-11-02 23:27:14 -0300

    ex

--- Ex-001.py
+++ Ex-001.py
@@ -1,9 +1,18 @@
 '''
-Crie um programa que escreva "Olá, Mundo!" na tela.
+Faça um programa que receba quatro números inteiros, calcule e mostre a soma desses números.
 Autor:Jules do Nascimento Pires
 Ex:001
-Data:01/10/2025
+Data:01/11/2025
 '''
 
-# Mensagem
-print("Olá, Mundo!")
+# Entrada de dados
+num1 = int(input('Primeiro valor:'))
+num2 = int(input('Segundo valor:'))
+num3 = int(input('Terçeiro valor:'))
+num4 = int(input('Quarto valor:'))
+
+# Soma
+soma = num1 + num2 + num3 + num4
+
+# Resultado
+print('A soma dos valores é:{}'.format(soma))
